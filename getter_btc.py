e7fb5184a7b7a5a036bfb516d2a2b4aa56876cea
SG_Muwa <motherlode.muwa@gmail.com>
2020-08-19 00:23:25 +0400

    Revert "Make getter_btc asynchronous"
    This reverts commit 2e6d88be7ab40ef49aa55edd795dfdb049509cc7.

--- getter_btc.py
+++ getter_btc.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
-import aiohttp
-import asyncio
+import requests
 
-async def get_btc(authkey: str):
+def get_btc(authkey: str):
     """
     Gets the current bitcoin rate from server.
     :param authkey: Authkey from pro-api.coinmarketcap.com.
     """
     try:
-        async with aiohttp.ClientSession() as session:
-            response = await session.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest', headers = {'X-CMC_PRO_API_KEY': authkey, 'Accept': 'application/json'}, params = {'id': 1})
-            j = (await response.json())['data']['1']
-            currency = list(j['quote'].keys())[0]
-            return {'currency': currency, 'price': j['quote'][currency]['price']}
+        j = requests.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest',
+            headers = {'X-CMC_PRO_API_KEY': authkey, 'Accept': 'application/json'},
+            params = {'id': 1}).json()['data']['1']
+        currency = list(j['quote'].keys())[0]
+        return {'currency': currency, 'price': j['quote'][currency]['price']}
     except:
         return None
 
-async def main():
-    print(await get_btc(input("authkey: ")))
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    print(get_btc(input("authkey: ")))
