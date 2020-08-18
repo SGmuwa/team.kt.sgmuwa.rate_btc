fbb583e44cea1c9666fa374ffd3ebbce05550915
SG_Muwa <motherlode.muwa@gmail.com>
2020-08-18 17:57:12 +0400

    Currency service: make interface

---
+++ currency_service.py
@@ -0,0 +1,16 @@
+
+class Currency_service:
+    async def get_all(self):
+        pass
+
+    async def get_last(self):
+        pass
+
+    def get_timer_interval(self):
+        pass
+
+    def set_timer_interval(self, seconds):
+        pass
+
+    async def update_now(self):
+        pass
