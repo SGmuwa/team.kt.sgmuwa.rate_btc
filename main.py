cb62d4dcd651d88e872da2de72dce17bf894fe4a
SG_Muwa <motherlode.muwa@gmail.com>
2020-08-18 23:34:30 +0400

    main: beatifully

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ from currency_service import Currency_service
 app = Flask(__name__)
 currency_service = Currency_service()
 
-@app.route('/btc/api/v1.0/currencies', methods = ['GET'])
+
+@app.route('/btc/api/v1.0/currencies', methods=['GET'])
 def get_currencies():
-    return jsonify( { 'currencies': currency_service.get_all() } )
+    return jsonify({'currencies': currency_service.get_all()})
 
-@app.route('/btc/api/v1.0/currencies/last', methods = ['GET'])
+
+@app.route('/btc/api/v1.0/currencies/last', methods=['GET'])
 def get_currency_last():
-    return jsonify( { 'currency': currency_service.get_last() } )
+    return jsonify({'currency': currency_service.get_last()})
 
-@app.route('/btc/api/v1.0/timer/<int:seconds>', methods = ['UPDATE'])
+
+@app.route('/btc/api/v1.0/timer/<int:seconds>', methods=['UPDATE'])
 def set_timer_interval(seconds: int):
     currency_service.interval = seconds
-    return jsonify( { 'interval': currency_service.interval } )
+    return jsonify({'interval': currency_service.interval})
 
-@app.route('/btc/api/v1.0/timer/now', methods = ['UPDATE'])
+
+@app.route('/btc/api/v1.0/timer/now', methods=['UPDATE'])
 def update_now():
-    return jsonify( { 'currency': currency_service.update_now() } )
+    return jsonify({'currency': currency_service.update_now()})
+
 
 if __name__ == '__main__':
-    app.run(debug = False)
+    app.run(debug=False)
