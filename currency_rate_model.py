0604c5f450359a66db8ddcaf12c1e8c326a6b760
SG_Muwa <motherlode.muwa@gmail.com>
2020-08-19 09:41:52 +0400

    Fix «TypeError: Object of type CurrencyRateModel is not JSON serializable»

--- currency_rate_model.py
+++ currency_rate_model.py
@@ -1,8 +1,10 @@
-
+from dataclasses import dataclass
 from sqlalchemy import Column, Integer, String, DateTime, Float
 from datetime import datetime
 
+
 def make_model(Base):
+    @dataclass
     class CurrencyRateModel(Base):
         __tablename__ = 'currency_rate_btc_history'
 
